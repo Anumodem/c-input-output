ef437514caa9bc0e7cc23001d4ab765e4623a703
Anumodem <110585951+Anumodem@users.noreply.github.com>
2022-12-18 17:31:47 +0530

    c

---
+++ question8.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+    float radiusofball,volumeofball;
+    scanf("%f",&radiusofball);
+    volumeofball=(4/3)*3.14*pow(radiusofball,3);
+    printf("%f",volumeofball);
+    
+
+	return 0;
+}
